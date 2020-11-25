0c3e0c18392abc816e4446542a5b182d2ce7178b
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2020-11-25 14:50:01 +0100

    Update Layout.jsx

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -5,6 +5,7 @@ function Layout(props) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title> {props.title ? props.title : "My App"} </title>
         <link rel="stylesheet" href="/stylesheets/style.css" />
       </head>
